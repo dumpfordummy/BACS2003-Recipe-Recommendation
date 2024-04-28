bfd8b822c36d35c8ba581a576135e96877c12ac3
cyhoo <cyhoo0528@gmail.com>
2024-04-28 20:34:58 +0800

    like button!!!!!

--- Engage-22-Project-Bongrub-main/Engage-22-Project-Bongrub-main/node app/public/recipe.js
+++ Engage-22-Project-Bongrub-main/Engage-22-Project-Bongrub-main/node app/public/recipe.js
@@ -4,3 +4,14 @@ for(let i=0;i<recipeRating;i++){
     ratingDiv.childNodes[2*i+1].classList.toggle("text-gray-300");
     ratingDiv.childNodes[2*i+1].classList.toggle("text-yellow-400");
 }
+
+
+function addToLocalStorage(recipeId) {
+    var likedRecipeList = JSON.parse(localStorage.getItem('likedRecipeList')) || [];
+    if (!likedRecipeList.includes(recipeId)) {
+        likedRecipeList = likedRecipeList.concat(recipeId);
+
+        localStorage.setItem('likedRecipeList', JSON.stringify(likedRecipeList));
+    }
+
+}
